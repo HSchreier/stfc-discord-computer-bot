fac741cbd6db04c89851a042434973b59a94e397
Helmut Schreier <6533040+HSchreier@users.noreply.github.com>
2025-07-28 20:45:38 +0200

    updates

--- src/utils/version.ts
+++ src/utils/version.ts
@@ -3,11 +3,12 @@ import config from '../config';
 export function getVersionInfo(): string {
   const versionInfo = `🖖 **${config.botName} v${config.version}**\n\n` +
     `**Current Version Features:**\n` +
-    `${config.roadmap.find(item => item.version === config.version)?.features}\n\n` +
+    `User data is anonymized via Crypto\n`+
     `**Roadmap:**\n` +
     config.roadmap.map(item =>
       `${item.status} **v${item.version}**: ${item.features}`
     ).join('\n') + '\n\n' +
+    `https://github.com/HSchreier/stfc-discord-computer-bot\n\n`+
     `_Live long and prosper._ 🖖`;
 
   return versionInfo;
